20aeb1149d48934c30a4db838b87e1c7c93b3230
Ibrahim Majid <i.majidbashir@gmail.com>
2025-01-23 23:57:51 -0500

    edited 128 so its more intuitive

--- Arrays & Hashing/128 Longest consequtive sequence.py
+++ Arrays & Hashing/128 Longest consequtive sequence.py
@@ -2,15 +2,16 @@ from collections import defaultdict
 
 
 def longestConsecutive(nums: list[int]) -> int:
-    mp = defaultdict(int)
-    res = 0
+    # basically we look to the left of each number, cuz if  it doesnt have
+    # anything to the left its a starting point and go from there
+    numSet = set(nums)
+    longest = 0
 
-    for num in nums:
-        if not mp[num]:
-            mp[num] = mp[num - 1] + mp[num + 1] + 1
-            mp[num - mp[num - 1]] = mp[num]
-            mp[num + mp[num + 1]] = mp[num]
-            res = max(res, mp[num])
-    return res
-
+    for num in numSet:
+        if (num - 1) not in numSet:
+            length = 1
+            while (num + length) in numSet:
+                length += 1
+            longest = max(length, longest)
+    return longest
 print(longestConsecutive([2,20,4,10,3,4,5]))
