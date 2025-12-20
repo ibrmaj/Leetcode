585d14aefbd0d524fc0410a0c2c4954122a2740e
Ibrahim Majid <i.majidbashir@gmail.com>
2025-12-20 17:11:54 -0500

    Fix typo in comment about area calculation
    Corrected a typo in the comment regarding the algebraic algorithm.

--- Two Pointers/container_with_most_water.py
+++ Two Pointers/container_with_most_water.py
@@ -9,10 +9,12 @@ class Solution:
             area = min(height[l], height[r]) * x # have to use the min as how are you gonna fill up a water bowl to the max wall? it does only till the min wall
 
             ans = max(area, ans)
-            # below is the main 'alegraic' algo that this q is testing us on, basically area = (r - l) * min(h1, h2)
+            # below is the main 'alegbraic' algo that this q is testing us on, basically area = (r - l) * min(h1, h2)
             # to maximise the area we can either increase the  r - l term or the min() term 
             # now the r - l term will be decreasing right but the min term can increase, and will only increase if we up the lower of the h1 and h2
             # thats what we do here, if height[l] is smaller then the l pointer goes up and vice versa, hoping to get max area
+
+            # Furthermore, the minimum of the l and r is acc what determines the area so the only way to see if area 'increases' is to move the lower of the both
             if height[l] < height[r]:
                 l += 1
             else:
